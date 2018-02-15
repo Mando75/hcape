2e26dc1d93d86eabf249734e23e20491e8083cb3
Bryan <starwarsgeek0975@gmail.com>
2018-02-15 11:16:54 -0700

    Intial layout completed

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -7,12 +7,22 @@ const {Header, Footer, Content} = Layout;
 export class App extends React.Component {
 
   render() {
-     return (<div>
-       <Layout className={'layout'}>
-         <Header><Nav/></Header>
-         <Content>Content</Content>
-         <Footer>Foot</Footer>
-       </Layout>
-     </div>)
+    return (
+        <Layout style={{minHeight: '100vh'}}>
+          <Nav/>
+          <Layout>
+            {/*<Header style={{ background: '#fff', padding: 0 }} />*/}
+            <Content style={{ margin: '24px 16px 0' }}>
+              <div style={{ padding: 24, background: '#fff', minHeight: '90vh' }}>
+                content
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+              Ant Design ©2016 Created by Ant UED
+            </Footer>
+          </Layout>
+        </Layout>
+
+    )
   }
 }
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import {Menu, Icon} from 'antd';
+import {Menu, Icon, Layout} from 'antd';
+const {Sider} = Layout;
 
 export class Nav extends React.Component {
+
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
+
   render() {
     return (
-        <div>
-          <div className={'logo'}><Icon type="info-circle" /></div>
-          <Menu theme={'dark'} mode={'horizontal'} style={{lineHeight: '64px'}}>
-          <Menu.Item  key={1}>Nav 1 </Menu.Item>
+        <Sider
+            breakpoint="lg"
+            collapsible
+            collapsed={this.state.collapsed}
+            onCollapse={this.toggle}
+        >
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu.Item key="1">
+            <Icon type="user" />
+            <span className="nav-text">nav 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="video-camera" />
+            <span className="nav-text">nav 2</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Icon type="upload" />
+            <span className="nav-text">nav 3</span>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Icon type="user" />
+            <span className="nav-text">nav 4</span>
+          </Menu.Item>
         </Menu>
-        </div>
+        </Sider>
     )
   }
 }
