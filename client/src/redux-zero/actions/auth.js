2a179ac4249d0c563e9fd95d0147d1f0db4cb625
Bryan <starwarsgeek0975@gmail.com>
2018-03-20 14:48:12 -0600

    added vue

--- client/src/redux-zero/actions/auth.js
+++
@@ -1,9 +0,0 @@
-import store from '../store';
-
-export const auth = () => {
-  store.setState({_authed: true});
-};
-
-export const deauth = () => {
-  store.setState({_authed: false})
-};
--- client/src/redux-zero/actions/loading.js
+++
@@ -1,9 +0,0 @@
-import store from '../store';
-
-export const startLoading = () => {
-  store.setState({_loading: true});
-};
-
-export const finishLoading = () => {
-  store.setState({_loading: false})
-};
