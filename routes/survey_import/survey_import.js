8bc0d0100589101e07edb10358152f2672275e92
Muller <mando0975@byui.edu>
2018-04-24 14:00:36 -0600

    intial router setup for imports

---
+++ routes/survey_import/survey_import.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const {checkt, validatonResult} = require('express-validator');
+const surveyImportRouter = express.Router();
+
+surveyImportRouter.post('/', (req, res) => {
+
+});
+
+surveyImportRouter.get('/status', (req, res) => {
+
+});
+
+surveyImportRouter.get('/:response_id', (req, res) => {
+
+});
