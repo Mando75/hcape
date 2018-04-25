e8a2ee6008a592e07a395b5c964875df2a3d282b
Muller <mando0975@byui.edu>
2018-04-25 15:01:08 -0600

    fetch survey data endpoint created

--- routes/api-root.js
+++ routes/api-root.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import {authRouter} from "./auth/auth";
+import {surveyImportRouter} from "./survey_import/survey_import";
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   const message = {
     message: 'api root'
   };
@@ -11,6 +12,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.use('/auth', authRouter);
+router.use('/survey_import', surveyImportRouter);
 
 
 export {router}
--- routes/survey_import/survey_import.js
+++ routes/survey_import/survey_import.js
@@ -1,16 +1,36 @@
 import express from 'express';
+// import axios from 'axios';
+import {axiosQualtrics} from "../../lib/qualtrics";
 
-const {checkt, validatonResult} = require('express-validator');
+const sanitize = require('sanitizer').sanitize;
+const {check, validatonResult} = require('express-validator');
 const surveyImportRouter = express.Router();
 
-surveyImportRouter.post('/', (req, res) => {
+
+surveyImportRouter.post('/export', (req, res) => {
 
 });
 
-surveyImportRouter.get('/status', (req, res) => {
+surveyImportRouter.get('/export/status', (req, res) => {
 
 });
 
 surveyImportRouter.get('/:response_id', (req, res) => {
 
 });
+
+surveyImportRouter.get('/survey/:survey_id', async (req, res) => {
+  const survey_id = sanitize(req.params.survey_id);
+  try {
+    const survey = await axiosQualtrics.get(`/surveys/${survey_id}`);
+    console.log(survey);
+    res.status(200).json(survey);
+  } catch (error) {
+    res.status(401).json(error);
+  }
+
+
+
+});
+
+export {surveyImportRouter}
