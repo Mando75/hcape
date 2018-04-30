3d154a0775f2c79b97e2c8fda425955785d4128e
Muller <mando0975@MCK355-160720.c.byui.edu>
2018-04-30 15:02:28 -0600

    the billy commit

--- resolvers/mongodb-connection.js
+++ resolvers/mongodb-connection.js
@@ -1,4 +1,4 @@
-import {MongoClient} from 'mongodb';
+import {MongoClient, ObjectID} from 'mongodb';
 
 /**
  * Use this function to connect to and make database queries.
@@ -8,13 +8,20 @@ import {MongoClient} from 'mongodb';
  * @returns {Promise<Collection>}
  */
 export async function connectToDb(collName = 'test', dbName = 'qe') {
-  const url = process.env.DATABASE_URL;
-  try {
-    const client = await MongoClient.connect(url);
-    // noinspection JSCheckFunctionSignatures
-    return client.db(dbName).collection(collName);
-  } catch (e) {
-    console.error('Warning: Connection to Database failed', e);
-  }
+    const url = process.env.DATABASE_URL;
+    try {
+        const client = await MongoClient.connect(url);
+        // noinspection JSCheckFunctionSignatures
+        return client.db(dbName).collection(collName);
+    } catch (e) {
+        console.error('Warning: Connection to Database failed', e);
+    }
 }
+
+export const COLLECTIONS = {
+    FACULTY: 'faculty',
+    STUDENT: 'student'
+};
+
+export const mongoId = id => new ObjectID(id);
 
--- routes/survey_import/survey_import.js
+++ routes/survey_import/survey_import.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {axiosQualtrics} from "../../lib/qualtrics";
 import {parse_survey_meta, parse_survey_questions} from "../../lib/parse_survey";
 import {fetch_qualtrics_survey_data} from "../../lib/qualtrics_api/fetch_survey";
+import {connectToDb, COLLECTIONS, mongoId} from "../../resolvers/mongodb-connection";
 
 const sanitize = require('sanitizer').sanitize;
 const {check, validatonResult} = require('express-validator');
@@ -25,21 +26,34 @@ surveyImportRouter.get('/:response_id', (req, res) => {
  * This data includes the survey name, question data, etc.
  */
 surveyImportRouter.get('/survey/:survey_id', async (req, res) => {
-  const survey_id = sanitize(req.params.survey_id);
-  const survey = fetch_qualtrics_survey_data(survey_id);
-  res.status(survey.status).json(survey.data);
+    const survey_id = sanitize(req.params.survey_id);
+    const survey = await fetch_qualtrics_survey_data(survey_id);
+    res.status(survey.status).json(survey.data || survey.error);
 });
 
 surveyImportRouter.post('/survey/:survey_id', async (req, res) => {
-  const survey_id = sanitize(req.params.survey_id);
-  //TODO Use fetch_qualtrics_survey
-    const survey = (await axiosQualtrics.get(`/surveys/${survey_id}`)).data;
-    const parsedSurvey = {
-      meta: parse_survey_meta(survey.result),
-      questions: parse_survey_questions(survey.result)
+    const auth = req.authpayload;
+    const survey_id = sanitize(req.params.survey_id);
+    const conn = await connectToDb(COLLECTIONS.FACULTY);
+    try {
+        const survey = (await axiosQualtrics.get(`/surveys/${survey_id}`)).data;
+        const parsedSurvey = {
+            meta: parse_survey_meta(survey.result),
+            questions: parse_survey_questions(survey.result)
+        };
 
-    //TODO write resolver function to import into database.
-  }
+        //TODO write resolver function to import into database.
+
+        const teach = await conn.findOneAndUpdate({_id: mongoId(auth._id)}, {fields: {email: 1, username: 1}});
+        console.log(teach);
+        // console.log(mongoId(auth._id))
+        res.send(teach);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json
+        (error);
+    }
+
 });
 
 export {surveyImportRouter}
