4dd0b70c7df2962a9d44348f4788843476ca6288
Muller <mando0975@byui.edu>
2018-05-02 10:17:13 -0600

    survey question import updated to insert into set to prevent duplicates

--- routes/survey_import/survey_import.js
+++ routes/survey_import/survey_import.js
@@ -26,34 +26,35 @@ surveyImportRouter.get('/:response_id', (req, res) => {
  * This data includes the survey name, question data, etc.
  */
 surveyImportRouter.get('/survey/:survey_id', async (req, res) => {
-    const survey_id = sanitize(req.params.survey_id);
-    const survey = await fetch_qualtrics_survey_data(survey_id);
-    res.status(survey.status).json(survey.data || survey.error);
+  const survey_id = sanitize(req.params.survey_id);
+  const survey = await fetch_qualtrics_survey_data(survey_id);
+  res.status(survey.status).json(survey.data || survey.error);
 });
 
 surveyImportRouter.post('/survey/:survey_id', async (req, res) => {
-    const auth = req.authpayload;
-    const survey_id = sanitize(req.params.survey_id);
-    const conn = await connectToDb(COLLECTIONS.FACULTY);
-    try {
-        const survey = (await axiosQualtrics.get(`/surveys/${survey_id}`)).data;
-        const parsedSurvey = {
-            meta: parse_survey_meta(survey.result),
-            questions: parse_survey_questions(survey.result)
-        };
+  const auth = req.authpayload;
+  const survey_id = sanitize(req.params.survey_id);
+  const conn = await connectToDb(COLLECTIONS.FACULTY);
+  try {
+    const survey = (await axiosQualtrics.get(`/surveys/${survey_id}`)).data;
+    const parsedSurvey = {
+      meta: parse_survey_meta(survey.result),
+      questions: parse_survey_questions(survey.result)
+    };
 
-        //TODO write resolver function to import into database.
+    //TODO write resolver function to import into database.
 
-        const teach = await conn.findOneAndUpdate({_id: mongoId(auth._id)},
-            {$push: { surveys: parsedSurvey }}, {returnNewDocument: true, returnOriginal: false});
-        const resp = {
-          status: teach.lastErrorObject.updatedExisting ? 200 : 500,
-          data: teach.value,
-        };
-        res.status(resp.status).json(resp);
-    } catch (error) {
-        res.status(401).send(error.response.statusText);
-    }
+    const teach = await conn.findOneAndUpdate({_id: mongoId(auth._id)},
+        {$addToSet: {surveys: parsedSurvey}}, {returnNewDocument: true, returnOriginal: false});
+    console.log(teach);
+    const resp = {
+      status: teach.lastErrorObject.updatedExisting ? 200 : 500,
+      data: teach.value,
+    };
+    res.status(resp.status).json(resp);
+  } catch (error) {
+    res.status(401).send(error);
+  }
 
 });
 
