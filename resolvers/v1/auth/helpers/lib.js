5704e14faaa3f5adc8213696a3340c8f6a6a42f8
Bryan Muller <mul13001@byui.edu>
2018-05-21 12:42:53 -0600

    consolidate files

--- resolvers/v1/auth/helpers/lib.js
+++ resolvers/v1/auth/helpers/lib.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+const sanitize = require('sanitizer').sanitize;
 
 /**
  * Compares hashed passwords to provided passwords.
@@ -29,3 +30,40 @@ export async function hashPwd(pwd) {
   const rounds = 12;
   return await bcrypt.hash(pwd, rounds);
 }
+
+
+/**
+ * Extracts login params from req body.
+ * If one or more of the keys is undefined
+ * @param body
+ * @returns {*}
+ */
+export const extractLoginData = (body) => {
+  try {
+    return {
+      username: sanitize(body.username),
+      password: sanitize(body.pwd),
+      type: sanitize(body.type)
+    }
+  } catch (e) {
+    return null;
+  }
+};
+/**
+ * Extracts data needed for creating account
+ * @param body
+ * @returns {*}
+ */
+export const extractAccountData = (body) => {
+  try {
+    return {
+      username: sanitize(body.username),
+      pwd: sanitize(body.pwd),
+      email: sanitize(body.email),
+      inumber: sanitize(body.inumber),
+      type: sanitize(body.type)
+    }
+  } catch (e) {
+    return null;
+  }
+};
